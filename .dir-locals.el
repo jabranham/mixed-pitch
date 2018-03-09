b374b0d47e33806fa1e595f2b2c4bffe9b96f75f
Alex Branham <branham@utexas.edu>
2018-03-09 17:02:52 -0600

    Add bug-reference info to dir-locals

---
+++ .dir-locals.el
@@ -0,0 +1,6 @@
+;;; Directory Local Variables
+;;; For more information see (info "(emacs) Directory Variables")
+
+((nil . ((fill-column . 70)
+         (bug-reference-bug-regexp . "#\\(?2:[0-9]+\\)")
+         (bug-reference-url-format . "https://github.com/jabranham/mixed-pitch/issues/%s"))))
